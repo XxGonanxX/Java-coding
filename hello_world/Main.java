1c3b0664fe370b752145ff9936c295c8addf56db
Alan Patricio González <patomorelia@hotmail.com>
2023-08-03 20:13:28 -0600

    Many new features added on the hello_world

--- hello_world/Main.java
+++ hello_world/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello World!!");
+    System.out.println("I am learning Java.");
+    System.out.println("This is kinda complex but not at all.");
+
+    System.out.println(30000);
+
+    // The comments are written as in C++ or in Rust. I'm starting
+    // To like Java
+    System.out.println("Now, a math equation: 5 + 5 = " + (5 + 5));
+
+
+    /*
+     * We should never forget the multi-line comments
+     */
+
+    String name = "Gonan";
+    int age = 20; // I'm getting old :(
+
+    System.out.println("My name is " + name + " and I'm " + age + " years old.");
+    
+    // I believe the code is self-explanatory. Just my name and age using variables.
+
+    
+    final int Realage = 20;
+
+    System.out.println("By the time I write this code, I'm " + Realage + " years." );
+
+    // These variables can be locked so no one can change them using Final.
+
+    /* A lot of things are similar to C++ and Rust, but once we get to int types it changes
+    *  It can have scientific numbers and not every type of number can fit on the int type,
+    *  for more info on thast check online.
+    */
+
     }
 }
